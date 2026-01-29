4ad3a63203fe3e8da83eb2c7c66f073b5e870ba3
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-01-29 15:50:19 +0000

    Add files via upload

--- js/product-detail.js
+++ js/product-detail.js
@@ -1,23 +1,44 @@
+// estimated delivery fees
+const feeBtn = document.getElementById("feeSelectorBtn");
 
+const feeOption = document.getElementById("deliveryLocation");
 
-
-
-// DELIVERY FEE JS
-
-const locationSelect = document.getElementById("deliveryLocation");
 const feeText = document.getElementById("deliveryFee");
 
-locationSelect.addEventListener("change", () => {
-  const fees = {
-    accra: "GHS 20",
-    kumasi: "GHS 25",
-    sunyani: "GHS 30",
-    techiman: "GHS 35",
-    brekum: "GHS 35",
-    tamale: "GHS 35",
-    takoradi: "GHS 30",
-    other: "GHS 40"
-  };
 
-  feeText.textContent = fees[locationSelect.value] || "—";
+const feeMap =
+{
+accra: "GHS 30",
+takoradi: "GHS 20",
+kumasi: "GHS 25",
+sunyani: "GHS 30",
+techiman: "GHS 30",
+brekum: "GHS 32",
+tamale: "GHS 35",
+other: "GHS 40",
+};
+
+// toggle drpdwn
+feeBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  feeOption.style.display = feeOption.style.display ===
+  "flex"? "none" : "flex"; 
+});
+
+// select location
+feeOption.querySelectorAll(".div").forEach(opt => {
+  opt.addEventListener("click", () => {
+    const location = opt.getAttribute("value");
+    const label = opt.textContent;
+
+    feeBtn.childNodes[0].textContent = label;
+    feeText.textContent = feeMap[location] || "_";
+
+    feeOption.style.display = "none";
+  });
+});
+
+// toggle off window outside click
+window.addEventListener("click", () => {
+  feeOption.style.display = "none";
 });
